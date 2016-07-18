2b70e7097ab4f8b3fb2aa50530cc71190b921421
vasarhelyia <vasarhelyi.agnes@ustream.tv>
2016-07-18 12:43:38 +0200

    Add dummy test

--- TestFastlaneTests/TestFastlaneTests.swift
+++ TestFastlaneTests/TestFastlaneTests.swift
@@ -22,8 +22,8 @@ class TestFastlaneTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+		XCTAssertTrue(1 == 1)
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
